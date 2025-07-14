7152797327a1deb27d13dfeff96eeea386d4f379
vadbash <vadusikvadim@gmail.com>
2025-07-14 12:52:19 +0200

    main folder

---
+++ DebtsCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DebtsCounter
+//
+//  Created by Mac on 14.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DebtsCounter/DebtsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebtsCounterApp.swift
+//  DebtsCounter
+//
+//  Created by Mac on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DebtsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
