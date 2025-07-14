7d269707d119a81464a0b9151c8a41ef54f9390b
vadbash <vadusikvadim@gmail.com>
2025-07-14 12:52:48 +0200

    debtsCounter tests

---
+++ DebtsCounterTests/DebtsCounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  DebtsCounterTests.swift
+//  DebtsCounterTests
+//
+//  Created by Mac on 14.07.2025.
+//
+
+import Testing
+
+struct DebtsCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
