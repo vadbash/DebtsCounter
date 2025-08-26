a5e205b62e525277336275325885169a0285991f
vadbash <vadusikvadim@gmail.com>
2025-08-26 14:38:29 +0200

    Change back button

--- DebtsCounter/DebtsCounterApp.swift
+++ DebtsCounter/DebtsCounterApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct DebtsCounterApp: App {
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        let backButtonAppearance = UIBarButtonItemAppearance()
+        backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray]
+        appearance.backButtonAppearance = backButtonAppearance
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().tintColor = .gray
+        
+    }
+
+    
     var body: some Scene {
         WindowGroup {
             WelcomeView()
         }
     }
 }
+
+
